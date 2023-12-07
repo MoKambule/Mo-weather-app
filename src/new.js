92dbc363557aa9a74f6e37e4cdaf468cc2861855
MoKambule <kambulemokgethwa@gmail.com>
2023-12-07 14:56:57 +0200

    Api integration

--- src/new.js
+++ src/new.js
@@ -1,8 +1,21 @@
+function updateweather(response) {
+  let temperatureElement = document.querySelector(".weather-app-temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+  let apiKey = "323b9fb4ca5t30fbc37551af865ea4ob";
+  let apiURL =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  axios.get(apiURL).then(updateweather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityelement = document.querySelector("#city");
   cityelement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElemnt = document.querySelector("#search-form");
